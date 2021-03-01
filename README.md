cbc2418918d9b3aa6af6883ec37f902a92ab25c9
Elizabeth Swann <elizabethswann@Elizabeths-MBP.hitronhub.home>
2021-03-01 06:21:40 -0800

    App.vue added

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bookmark
+
+This is an app for managing URLs designed to learn VueJS.
+
+## Getting Started
+
+1. Download this repository and run `npm install`.
+2. To start the server run `npm run serve`.
+
+## Stack
+
+- VueJs
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        { 
+          id: 'offical-guide', 
+          title: 'Offical Guide',
+          description: 'The offical Vue.js documentation',
+          link: "https://vuejs.org"  
+        },
+        {
+          id: 'google', 
+          title: 'Google',
+          description: 'The offical guide to anything',
+          link: "https://google.com"  
+        }
+      ]
+    }
+  }
+}
+</script>
