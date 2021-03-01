910316b14f9b175f1607630468a46b7992ce610c
Elizabeth Swann <elizabethswann@Elizabeths-MBP.hitronhub.home>
2021-03-01 06:44:15 -0800

    data appearing on site

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <ul>
+    <resource-item v-for="res in storedResources" 
+    :key="res.id" 
+    :title='res.title'
+    :description="res.description"
+    :link="res.link"
+    >
+    </resource-item> 
   </ul>
 </template>
 
 <script>
+import ResourceItem from './components/resources/ResourceItem'
 export default {
+  components: {
+    ResourceItem,
+  },
   data() {
     return {
       storedResources: [
---
+++ src/components/resources/ResourceItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li>
+    <header>
+      <h3>{{ title }}</h3>
+      <button>Delete</button>
+    </header>
+    <p>{{ description }}</p>
+    <a :href='link'></a>
+  </li>
+</template>
+<script>
+export default {
+  props: [
+    'title', 'description', 'link'
+  ]
+}
+</script>
