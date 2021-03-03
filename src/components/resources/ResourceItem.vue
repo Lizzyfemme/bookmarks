12915b6bc771a6a13f4d366603489aff52bb6265
Elizabeth Swann <elizabethswann@iPhone.hitronhub.home>
2021-03-02 20:08:01 -0800

    styles added

--- src/components/resources/ResourceItem.vue
+++ src/components/resources/ResourceItem.vue
@@ -5,7 +5,7 @@
       <button>Delete</button>
     </header>
     <p>{{ description }}</p>
-    <a :href='link'></a>
+    <a :href="link">View Resource</a>
   </li>
 </template>
 <script>
@@ -15,3 +15,36 @@ export default {
   ]
 }
 </script>
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+  width:200px;
+  height:75px;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
